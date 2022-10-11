b7802eb326c610d71fbb9575de7465baed4d21cc
kuzhi23 <9size.marvel@gmail.com>
2022-10-11 14:52:08 +0900

    배열 CRUD

---
+++ chapter01/src/sampleData/smaple.js
@@ -0,0 +1,20 @@
+export const userData = [
+  {
+    id: 1,
+    username: "marvel_net",
+    email: "marvel_net@naver.com",
+    active: true,
+  },
+  {
+    id: 2,
+    username: "kuzhi",
+    email: "kuzhi@naver.com",
+    active: false,
+  },
+  {
+    id: 3,
+    username: "9xize",
+    email: "9xize@naver.com",
+    active: false,
+  },
+];
