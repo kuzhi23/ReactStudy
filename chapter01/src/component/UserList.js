620e8200200c8e6ad05cfddf0c503e29018207b7
kuzhi23 <9size.marvel@gmail.com>
2022-10-12 14:19:56 +0900

    useEffect, useMemo, useCallback React.memo

--- chapter01/src/component/UserList.js
+++ chapter01/src/component/UserList.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user, onRemove, onToggle }) {
   return (
     <div>
       <b
@@ -14,7 +14,7 @@ function User({ user, onRemove, onToggle }) {
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
-}
+});
 
 function UserList({ users, onRemove, onToggle }) {
   return (
@@ -31,4 +31,4 @@ function UserList({ users, onRemove, onToggle }) {
   );
 }
 
-export default UserList;
+export default React.memo(UserList);
